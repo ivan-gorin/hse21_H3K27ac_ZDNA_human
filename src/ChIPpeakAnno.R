21633acc8b05fd623833782f382d68268da0fda4
ivan-gorin <43433603+ivan-gorin@users.noreply.github.com>
2021-06-09 19:51:43 +0300

    genes

--- src/ChIPpeakAnno.R
+++ src/ChIPpeakAnno.R
@@ -1,4 +1,5 @@
-
+cwd = 'C:\\Users\\Ivan\\Documents\\hse21_H3K4me1_G4_human\\src'
+setwd(cwd)
 source('lib.R')
 
 ###
@@ -17,7 +18,7 @@ library(org.Hs.eg.db)
 ###
 
 
-peaks <- toGRanges(paste0(DATA_DIR, 'H3K4me3_A549.intersect_with_DeepZ.bed'), format="BED")
+peaks <- toGRanges(paste0(DATA_DIR, 'H3K4me1_A549.intersect_with_G4.bed'), format="BED")
 peaks[1:2]
 
 annoData <- toGRanges(TxDb.Hsapiens.UCSC.hg19.knownGene)
@@ -34,10 +35,10 @@ anno$symbol <- xget(anno$feature, org.Hs.egSYMBOL)
 data.frame(anno) %>% head()
 
 anno_df <- data.frame(anno)
-write.table(anno_df, file=paste0(DATA_DIR, 'H3K4me3_A549.intersect_with_DeepZ.genes.txt'),
+write.table(anno_df, file=paste0(DATA_DIR, 'H3K4me1_A549.intersect_with_G4.genes.txt'),
             col.names = TRUE, row.names = FALSE, sep = '\t', quote = FALSE)
 
 uniq_genes_df <- unique(anno_df['symbol'])
-write.table(uniq_genes_df, file=paste0(DATA_DIR, 'H3K4me3_A549.intersect_with_DeepZ.genes_uniq.txt'),
+write.table(uniq_genes_df, file=paste0(DATA_DIR, 'H3K4me1_A549.intersect_with_G4.genes_uniq.txt'),
             col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
 
